1e0fa2643239ff7a706ce1a4f64b84565d546c65
Hammad Mansoor <hammadmansoor75@gmail.com>
2023-09-12 12:30:50 +0500

    Project Initiation

---
+++ app/src/main/java/com/hammadmansoor/i200929/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hammadmansoor.i200929;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
