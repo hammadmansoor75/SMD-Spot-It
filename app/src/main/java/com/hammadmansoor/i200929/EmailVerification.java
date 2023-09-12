585de899fc67877996cd261718f40dc5b8b244f7
Hammad Mansoor <hammadmansoor75@gmail.com>
2023-09-12 16:51:49 +0500

    Email Verification Screen

---
+++ app/src/main/java/com/hammadmansoor/i200929/EmailVerification.java
@@ -0,0 +1,19 @@
+package com.hammadmansoor.i200929;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class EmailVerification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_verification);
+    }
+
+}
--- app/src/main/java/com/hammadmansoor/i200929/Registration.java
+++ app/src/main/java/com/hammadmansoor/i200929/Registration.java
@@ -2,7 +2,11 @@ package com.hammadmansoor.i200929;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Registration extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class Registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        Button signup = findViewById(R.id.signupButton);
+        signup.setOnClickListener(new View.OnClickListener(){
+            public void onClick (View view){
+                Intent intent = new Intent(Registration.this, EmailVerification.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView login = findViewById(R.id.loginClickable);
+        login.setOnClickListener(new View.OnClickListener(){
+            public void onClick (View view){
+                Intent intent = new Intent(Registration.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
